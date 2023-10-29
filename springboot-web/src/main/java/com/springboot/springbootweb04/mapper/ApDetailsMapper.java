d9934056071ae16a73d4eee8fabae92a404a4fde
lia <lwan540@aucklanduni.ac.nz>
2023-10-29 23:15:28 +1300

    D4

--- springboot-web/src/main/java/com/springboot/springbootweb04/mapper/ApDetailsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.springbootweb04.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.springboot.springbootweb04.entity.ApDetails;
-
-public interface ApDetailsMapper extends BaseMapper<ApDetails> {
-}
