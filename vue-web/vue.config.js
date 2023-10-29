fc95aa32690b09fc167be487e87e802c4b890fb8
lia <lwan540@aucklanduni.ac.nz>
2023-10-29 23:12:31 +1300

    1

--- vue-web/vue.config.js
+++
@@ -1,9 +0,0 @@
-// const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  transpileDependencies: true,
-  lintOnSave: false,
-  devServer: {
-      allowedHosts: 'all',
-    },
-
-}
